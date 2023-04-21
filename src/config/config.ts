085bcea119bdf724f93de33ad2a1e953fb18524a
Faizan Mansuri <127755890+FaizanSolulab@users.noreply.github.com>
2023-04-21 12:05:41 +0530

    two step verification channels implemented
    user can now choose the channel they want to receive otp on

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,28 +1,19 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
-
 export const TWILIO = {
-    serviceID: process.env.TWILIO_SERVICE_ID,
-    accoundSID: process.env.TWILIO_ACCOUNT_SID,
-    authToken: process.env.TWILIO_AUTH_TOKEN,
-}
-
-export const dbConnection = {
-    
-    db: {
-        
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-      },
-      listPerPage: 10,
-    
+  serviceID: process.env.TWILIO_SERVICE_ID,
+  accoundSID: process.env.TWILIO_ACCOUNT_SID,
+  authToken: process.env.TWILIO_AUTH_TOKEN,
 };
 
-
-
-
-
+export const dbConnection = {
+  db: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+  listPerPage: 10,
+};
